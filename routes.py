435440d98400aa17226fc8127cf8110eb53bc457
Siddharth <sidd.scope@gmail.com>
2014-12-03 20:42:37 +0530

    image gallery and he says she says

--- routes.py
+++ routes.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template('index.html')
+
+@app.route('/rsvp',methods=['GET','POST'])
+def rsvp():
+    if request.method == 'POST':
+        return "Thank You"
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', debug=True)
