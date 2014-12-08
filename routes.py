17a2e5ebd7bdbe65bfc68988f53c09a533cb7ff2
ishita345 <ishita.shah412@gmail.com>
2014-12-07 20:58:19 -0800

    post form

--- routes.py
+++ routes.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def home():
 	return render_template('index.html')
 
+@app.route('/rsvp/', methods=['POST'])
+def rsvp():
+    print request
+    name = request.form['name']
+    return json.dumps({'status':'OK','name':name});
+
+
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', debug=True)
