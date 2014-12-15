8fa06216426a569c52480e7292aa7afc2a51c3cc
ishita345 <ishita.shah412@gmail.com>
2014-12-15 00:51:54 -0800

    RSVP Form capture data and write into individual files.

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from flask import Flask, render_template, request
 
 weddingapp = Flask(__name__)
@@ -7,10 +8,20 @@ weddingapp = Flask(__name__)
 def home():
 	return render_template('index.html')
 
-@weddingapp.route('/rsvp/', methods=['POST'])
+@weddingapp.route('/rsvp', methods=['POST'])
 def rsvp():
     print request
+    print request.args
     name = request.form.get('name')
+    email = request.form.get('email')
+    people = request.form.get('people')
+    event = request.form.get('event')
+
+
+    filename = name.replace(" ", "_") + "---" + datetime.datetime.now().time().isoformat()
+    with open(filename, 'w') as f:
+        f.write(name + ", " + email + ", " + people + ", " + event + "\n")
+    f.closed
     return json.dumps({'status':'OK','name':name});
 
 
